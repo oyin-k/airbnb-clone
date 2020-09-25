9b4a111f2d1f32cd6154c6cd57c1e48cf7ca9afe
oyin-k <oyink7@gmail.com>
2020-09-26 00:06:22 +0100

    hamburger menu done

--- oy-airbnb-clone/src/components/Header.jsx
+++ oy-airbnb-clone/src/components/Header.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import SearchIcon from "@material-ui/icons/Search";
-import LanguageIcon from "@material-ui/icons/Language";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { Avatar } from "@material-ui/core";
 
 import "./Header.css";
 import logo from "../airbnblogo.png";
+import RightNav from "./RightNav";
+import Burger from "./Burger";
 
 function Header({ value, handleChange }) {
   return (
@@ -27,16 +26,8 @@ function Header({ value, handleChange }) {
         />
         <SearchIcon />
       </div>
-
       <div className="header__right">
-        <p>Become a host</p>
-        <div className="header__rightLanguage">
-          <LanguageIcon />
-          <ExpandMoreIcon />
-        </div>
-        <div className="header__rightAvatar">
-          <Avatar />
-        </div>
+        <Burger />
       </div>
     </div>
   );
---
+++ oy-airbnb-clone/src/components/RightNav.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+
+import styled from "styled-components";
+
+const StyledRightNav = styled.div`
+  display: flex;
+
+  @media screen and (max-width: 768px) {
+    /* display: none; */
+    flex-flow: column nowrap;
+    box-shadow: -10px 0 20px 0 rgba(0, 0, 0, 0.07);
+    background-color: white;
+    position: fixed;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 15rem;
+    padding: 2rem;
+    padding-top: 4rem;
+    transition: transform 0.3s ease-in-out;
+    z-index: 5000;
+    justify-content: flex-start;
+  }
+`;
+
+const RightNav = ({ open }) => {
+  return (
+    <StyledRightNav open={open}>
+      <p>Become a host</p>
+      <div className="header__rightLanguage">
+        <LanguageIcon />
+        <ExpandMoreIcon />
+      </div>
+      <div className="header__rightAvatar">
+        <Avatar />
+      </div>
+    </StyledRightNav>
+  );
+};
+
+export default RightNav;
